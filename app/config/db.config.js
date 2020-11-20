277ad680cc9307b385cab19360d1c31e0fdb24a2
maldee <maliciousgemba@gmail.com>
2020-11-20 18:12:24 +0530

    fff

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   HOST: "us-cdbr-east-02.cleardb.com",
-//   USER: "b1717e0b5390c6",
-//   PASSWORD: "8eff94fd",
-//   DB: "heroku_7814f402e2e3662",
-  
-// };
-
-
 module.exports = {
+   HOST: "us-cdbr-east-02.cleardb.com",
+   USER: "b1717e0b5390c6",
+   PASSWORD: "8eff94fd",
+   DB: "heroku_7814f402e2e3662",
+  
+ };
+
+
+/* module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
   DB: "deefire",
   
-};
+}; */
